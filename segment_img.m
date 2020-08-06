f05cb994e1d3cec5718a0d8c1b777ea99cd0d5e9
Devon Ulrich <dpulrich@gmail.com>
2020-08-06 16:16:25 -0400

    Added simple script for segmenting all images

---
+++ segment_img.m
@@ -0,0 +1,30 @@
+load('results/03-Aug-2020-12-51-53.mat');
+
+pplDS = imageDatastore("../images/*_ppl.tif");
+xplDS = imageDatastore("../images/*_xpl.tif");
+
+imCount = 1
+while hasdata(pplDS)
+    img = read(pplDS);
+    img(:,:,4:6) = read(xplDS);
+
+    output = uint8(zeros(size(img,1),size(img,2)));
+    imgscores = zeros(size(img,1),size(img,2));
+
+    for row = 1:256:(size(img,1) - 256)
+        currRows = row:(row+255);
+        for col = 1:256:(size(img,2) - 256)
+            currCols = col:(col+255);
+            
+            miniIm = img(currRows, currCols, :);
+            [C, scores, allscores] = semanticseg(miniIm, cnn_net);
+            clean = medfilt2(uint8(C), [5 5]);
+            
+            output(currRows, currCols) = clean;
+            imgscores(currRows, currCols) = scores;
+        end
+    end
+
+    save("/scratch/network/dulrich/segmented/" + imCount + "_out.mat", 'output', 'imgscores');
+    imCount = imCount + 1
+end
